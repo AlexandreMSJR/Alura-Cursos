3c21bee4e3323992e3d33bbdbca28ccd704ac4dc
Alexandre Medeiros <alexandremedeirosjunior@gmail.com>
2023-01-16 23:27:08 -0300

    Insert and selectSort

--- JavaScript/Algoritmos de Ordenação/selectionSort.js
+++ JavaScript/Algoritmos de Ordenação/selectionSort.js
@@ -6,10 +6,10 @@ for (let atual = 0; atual  < livros.length - 1; atual++) {
     let menor = menorValor(livros, atual);
 
     let livroAtual = livros[atual];
-    console.log('Posição Atual',atual)
-    console.log('Livro Atual',livroAtual)
+    // console.log('Posição Atual',atual)
+    // console.log('Livro Atual',livroAtual)
     let livroMenorPreco = livros[menor];
-    console.log('Livro menor preço',livroAtual)
+    // console.log('Livro menor preço',livroAtual)
 
     livros[atual] = livroMenorPreco
     livros[menor] = livroAtual
